1855dce91b5cf70349718db55666fe8f6b6155ae
Jacob Miller <Jamiller219@gmail.com>
2026-05-30 13:30:27 -0400

    log: add logging and use specialized plotly render function

--- Helpers/server.py
+++ Helpers/server.py
@@ -1,7 +1,9 @@
 import zen_mapper as zm
 from shiny import Inputs, Outputs, Session, reactive, render
-from shinywidgets import render_widget
+from shinywidgets import render_plotly
 from zen_mapper.types import MapperResult
+from logging import getLogger
+logger = getLogger(__name__)
 
 from Helpers.results import (
     ClustererResult,
@@ -42,6 +44,8 @@ def make_server(
             module_id = input.dataset_select()
             mod = dataset_modules[module_id]
             params = {p.id: input[f"{module_id}__{p.id}"]() for p in mod.PARAMS}
+
+            logger.info(f"UPDATING DATASET: \n {module_id}: {params}")
             return mod.result(
                 params=params, modules=dataset_modules, module_id=module_id
             )
@@ -52,6 +56,7 @@ def make_server(
             module_id = input.filter_select()
             mod = filter_modules[module_id]
             params = {p.id: input[f"{module_id}__{p.id}"]() for p in mod.PARAMS}
+            logger.info(f"UPDATING FILTER: \n {module_id}: {params}")
             return mod.result(
                 dataset=dataset,
                 params=params,
@@ -64,6 +69,7 @@ def make_server(
             module_id = input.cover_select()
             mod = cover_modules[module_id]
             params = {p.id: input[f"{module_id}__{p.id}"]() for p in mod.PARAMS}
+            logger.info(f"UPDATING COVER: \n {module_id}: {params}")
             return mod.result(
                 data=current_dataset(),
                 filtered=current_filtered_dataset(),
@@ -77,6 +83,7 @@ def make_server(
             module_id = input.cluster_select()
             mod = cluster_modules[module_id]
             params = {p.id: input[f"{module_id}__{p.id}"]() for p in mod.PARAMS}
+            logger.info(f"UPDATING CLUSTERER: \n {module_id}: {params}")
             return mod.result(
                 data=current_dataset(),
                 filtered=current_filtered_dataset(),
@@ -88,6 +95,7 @@ def make_server(
 
         @reactive.calc
         def current_mapper_result() -> MapperResult:
+            logger.info("UPDATING MAPPER RESULT...")
             return zm.mapper(
                 data=current_dataset().data,
                 projection=current_filtered_dataset().filtered_data,
@@ -98,6 +106,7 @@ def make_server(
 
         @reactive.calc
         def current_context() -> Context:
+            logger.info("UPDATING CONTEXT...")
             return Context(
                 dataset=current_dataset(),
                 filter=current_filtered_dataset(),
@@ -113,6 +122,7 @@ def make_server(
         def _auto_update_ui():
             for mod_id, mod in all_modules.items():
                 if hasattr(mod, "update_ui"):
+                    logger.info(f"AUTO UPDATING UI FOR {mod_id}...")
                     mod.update_ui(
                         ctx=current_context(),
                         mapper_result=current_mapper_result(),
@@ -129,16 +139,24 @@ def make_server(
             @render.plot
             def _matplotlib_renderer():
                 if mod_id not in (input.visualization_select() or []):
+                    logger.info(f"{mod_id} is not selected, returning None")
                     return None
 
                 # if no render_matplotlib return None
                 if not hasattr(visualization_modules[mod_id], "render_matplotlib"):
+                    logger.info(
+                            f"{mod_id}: render_matplotlib function does \
+                                    not exist, returning None"
+                            )
                     return None
 
                 ctx = current_context()
                 mapper_result = current_mapper_result()
                 params = {p.id: input[f"{mod_id}__{p.id}"]() for p in mod.PARAMS}
                 if mod.render_matplotlib:
+                    logger.info(
+                            f"{mod_id}: RENDERING matplotlib plot."
+                            )
                     return mod.render_matplotlib(
                         ctx=ctx,
                         mapper_result=mapper_result,
@@ -146,12 +164,17 @@ def make_server(
                     )
 
             @output(id=f"plotly__{mod_id}")
-            @render_widget
+            @render_plotly
             def _plotly_renderer():
                 if mod_id not in (input.visualization_select() or []):
+                    logger.info(f"{mod_id} is not selected, returning None")
                     return None
                 # if no render_plotly return None
                 if not hasattr(visualization_modules[mod_id], "render_plotly"):
+                    logger.info(
+                            f"{mod_id}: render_plotly function does \
+                                    not exist, returning None"
+                            )
                     return None
 
                 ctx = current_context()
@@ -159,6 +182,9 @@ def make_server(
                 params = {p.id: input[f"{mod_id}__{p.id}"]() for p in mod.PARAMS}
 
                 if mod.render_plotly:
+                    logger.info(
+                            f"{mod_id}: RENDERING plotly."
+                            )
                     return mod.render_plotly(
                         ctx=ctx,
                         mapper_result=mapper_result,
